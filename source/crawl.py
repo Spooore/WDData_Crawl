80a8bc6e4568c884ef5f0cb5aeb25b4eeab2437b
Spooore <erward.erward@gmail.com>
2019-03-20 20:13:47 +0100

    csv made

--- source/crawl.py
+++ source/crawl.py
@@ -1,10 +1,12 @@
 import os
 import numpy as np     
 import csv
+import requests
+import xml.etree.ElementTree as ET
 
 takeout = "../Takeout/Muzyka Google Play/Playlisty"
 csv_out = '../Consolidated.csv'
-csv_header = 'Tytuł,Album,Artysta,Czas trwania (ms),Ocena,Liczba odsłuchań,Usunięto,Indeks playlisty'
+csv_header = 'Tytuł,Album,Artysta,Czas trwania (ms),Ocena,Liczba odsłuchań,Usunięto,Indeks playlisty,Genre'
 
 
 CSVFiles = []
@@ -26,8 +28,23 @@ for csvFile in CSVFiles:
         next(reader, None)
         for row in reader:
             if row[0] is not "":
-                csv_merge.write(','.join(row).replace('&quot;','').replace('&amp;','').replace('&#39;',''))
-                csv_merge.write('\n')
+                line = ','.join(row).replace('&quot;','').replace('&amp;','').replace('&#39;','')
+                line_splited=line.split(",")
+                print(line_splited[2] + "////" + line_splited[0])
+                request="http://ws.audioscrobbler.com/2.0/?method=track.getinfo&api_key=b25b959554ed76058ac220b7b2e0a026&artist="+line_splited[2]+"&track="+line_splited[0]
+                response = requests.get(request)
+                tree = ET.ElementTree(ET.fromstring(response.content))
+                for child in tree.getroot().findall('./track/toptags/tag'):
+                    genre = child.find('name')
+                    csv_merge.write(line)
+                    csv_merge.write(",")
+                    csv_merge.write(genre.text) 
+                    csv_merge.write("\n")
+                if len(tree.getroot().findall('./track/toptags/tag'))==0:
+                    csv_merge.write(line)
+                    csv_merge.write(",")
+                    csv_merge.write("other")
+                    csv_merge.write("\n")
 
  
 print(csv_merge)
