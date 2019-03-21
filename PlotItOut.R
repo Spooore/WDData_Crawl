adda97e7b2dc229ac67e688f0b3e83b011859ea1
Spooore <erward.erward@gmail.com>
2019-03-21 22:29:53 +0100

    plot ready

--- PlotItOut.R
+++ PlotItOut.R
@@ -5,3 +5,18 @@ Consolidated <- read_delim("Documents/PythonProjects/WDGTDC/WDData_Crawl/Consoli
                            "#", escape_double = FALSE, trim_ws = TRUE)
 View(Consolidated)
 
+Consolidated<-Consolidated %>% 
+  mutate(Genre=tolower(Genre)) %>% 
+  mutate(Genre=sub(" ","",Genre))
+
+PlotData<-Consolidated %>% 
+  group_by(`Genre`) %>% 
+  summarise(listean_time = sum(`Liczba odsłuchań`)) %>% 
+  data.frame()  %>%
+  top_n(15)
+
+ggplot(data=PlotData, aes(x = Genre, y = listean_time)) +
+  scale_x_discrete() +
+  scale_y_continuous() + 
+  geom_bar(stat='identity') +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))
