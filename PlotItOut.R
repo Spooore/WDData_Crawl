9e7c4f469f1384689c216322028225ca6f724e4f
Spooore <erward.erward@gmail.com>
2019-03-24 17:49:38 +0100

    color and sorint

--- PlotItOut.R
+++ PlotItOut.R
@@ -1,6 +1,7 @@
 library(ggplot2)
 library(dplyr)
 library(readr)
+library(forcats)
 Consolidated <- read_delim("Documents/PythonProjects/WDGTDC/WDData_Crawl/Consolidated.csv", 
                            "#", escape_double = FALSE, trim_ws = TRUE)
 View(Consolidated)
@@ -15,8 +16,9 @@ PlotData<-Consolidated %>%
   data.frame()  %>%
   top_n(15)
 
-ggplot(data=PlotData, aes(x = Genre, y = listean_time)) +
+ggplot(data=PlotData, aes(x = reorder(Genre,-listean_time), y = listean_time, fill = listean_time)) +
   scale_x_discrete() +
   scale_y_continuous() + 
   geom_bar(stat='identity') +
-  theme(axis.text.x = element_text(angle = 45, hjust = 1))
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))+
+  scale_fill_gradient(low = "yellow", high = "red")
